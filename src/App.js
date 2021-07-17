643e82dbe9b35bb58ca88ef8b963ebbc8102b171
ryanyuri357 <ryan.yuri357@gmail.com>
2021-07-17 18:22:35 -0500

    Update b

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Navbar from "./Navbar";
 import "./styles.css";
-
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import PortfolioSection from "./PortfolioSection";
 
 const App = () => {
-    return(
-        <Navbar />
-
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <PortfolioSection />
+    </div>
+  );
+};
 
 export default App;
